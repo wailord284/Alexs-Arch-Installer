4272a97b7b55544eed1f086fd3d598812847171c
Alex Gaudino <wailord284@gmail.com>
2020-12-26 22:33:15 -0800

    Remove accidental #

--- install.sh
+++ install.sh
@@ -728,7 +728,7 @@ mv Arch-Linux-Installer-master/configs/networkmanager/dns-servers.conf /mnt/etc/
 mv Arch-Linux-Installer-master/configs/networkmanager/no-systemd-resolve.conf /mnt/etc/NetworkManager/conf.d/
 #Create one time ntpdupdate + hwclock to set date
 mkdir -p /mnt/etc/systemd/system/ntpdate.service.d
-mv Arch-Linux-Installer-master/configs/networkmanager/hwclock.conf /mnt/etc/systemd/system/ntpdate.service.d/#
+mv Arch-Linux-Installer-master/configs/networkmanager/hwclock.conf /mnt/etc/systemd/system/ntpdate.service.d/
 #Allow user in the network group to add/modify/delete networks without a password
 #mv -f Arch-Linux-Installer-master/configs/polkit-1/50-org.freedesktop.NetworkManager.rules /mnt/etc/polkit-1/rules.d/
 
