8427e64ee9eeb76d555f38419d0edd6e52ac66cc
Alex Gaudino <wailord284@gmail.com>
2022-03-15 21:34:18 -0700

    Add escape characters for environment variables

--- install.sh
+++ install.sh
@@ -857,19 +857,19 @@ sed "s,PKGEXT='.pkg.tar.zst',PKGEXT='.pkg.tar',g" -i /mnt/etc/makepkg.conf
 ###ENVIRONMENT VARIABLES###
 cat << EOF >> /mnt/etc/profile
 #Environment variables to move files out of home directory
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="\$HOME/.config"
+export XDG_DATA_HOME="\$HOME/.local/share"
+export XDG_CACHE_HOME="\$HOME/.cache"
 
-export GTK2_RC_FILES="${XDG_CONFIG_HOME}/gtk-2.0/gtkrc"
-export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
-export INPUTRC="${XDG_CONFIG_HOME}/readline/inputrc"
-export LESSHISTFILE="${XDG_CACHE_HOME}/less/history"
-export LESSKEY="${XDG_CONFIG_HOME}/less/lesskey"
-export npm_config_cache="${XDG_CACHE_HOME}/npm"
-export SCREENRC="${XDG_CONFIG_HOME}/screen/screenrc"
-export CARGO_HOME="${XDG_CACHE_HOME}/cargo"
-export GIMP2_DIRECTORY="${XDG_CONFIG_HOME}/gimp"
+export GTK2_RC_FILES="\${XDG_CONFIG_HOME}/gtk-2.0/gtkrc"
+export GNUPGHOME="\${XDG_DATA_HOME}/gnupg"
+export INPUTRC="\${XDG_CONFIG_HOME}/readline/inputrc"
+export LESSHISTFILE="\${XDG_CACHE_HOME}/less/history"
+export LESSKEY="\${XDG_CONFIG_HOME}/less/lesskey"
+export npm_config_cache="\${XDG_CACHE_HOME}/npm"
+export SCREENRC="\${XDG_CONFIG_HOME}/screen/screenrc"
+export CARGO_HOME="\${XDG_CACHE_HOME}/cargo"
+export GIMP2_DIRECTORY="\${XDG_CONFIG_HOME}/gimp"
 EOF
 
 
