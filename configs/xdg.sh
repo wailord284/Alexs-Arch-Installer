ef884dd7160d70d863f0a7239bdab2cd076e0790
Alex Gaudino <wailord284@gmail.com>
2022-08-05 20:02:39 -0700

    Update xdg variables

--- configs/xdg.sh
+++ configs/xdg.sh
@@ -4,13 +4,13 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_STATE_HOME="$HOME/.local/state"
 
-export ICEAUTHORITY="$XDG_CACHE_HOME"/ICEauthority
-export ERRFILE="${XDG_CACHE_HOME}/X11/xsession-errors"
+export ICEAUTHORITY="$XDG_CACHE_HOME/ICEauthority"
+export ERRFILE="$XDG_CACHE_HOME/X11/xsession-errors"
 export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
 export GTK2_RC_FILES="${XDG_CONFIG_HOME}/gtk-2.0/gtkrc"
 export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
 export INPUTRC="${XDG_CONFIG_HOME}/readline/inputrc"
-export LESSHISTFILE="${XDG_CACHE_HOME}/less/history"
+export LESSHISTFILE="$XDG_CACHE_HOME/less/history"
 export LESSKEY="${XDG_CONFIG_HOME}/less/lesskey"
 export npm_config_cache="${XDG_CACHE_HOME}/npm"
 export SCREENRC="$XDG_CONFIG_HOME/screen/screenrc"
