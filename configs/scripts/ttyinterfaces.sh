020d08801dd397915fafab83a214e06dd757b0f2
Alex Gaudino <wailord284@gmail.com>
2022-09-05 22:25:32 -0700

    Improve tty interfaces script

--- configs/scripts/ttyinterfaces.sh
+++ configs/scripts/ttyinterfaces.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
-#Wait 60 seconds in case the user needs to login or the network needs to connect
-sleep 60s
-#Add the default Arch prompt and clear the old addresses
+#Add the default Arch prompt and clear the old prompts
 echo "Arch Linux \r (\l)" > /etc/issue
 echo "Hostname: \n" >> /etc/issue
 echo "Today is: \d" >> /etc/issue
 echo "The time is: \t" >> /etc/issue
 echo "" >> /etc/issue
-#Output a list of all interfaces to /etc/issue
-for interface in $(ls /sys/class/net/) ; do
-	echo "IPv4 Address for $interface: \4{$interface}" >> /etc/issue
+
+#Use our own network connectivity check
+#Check if we can access archlinux.com 10 tens, once a minute
+#If after 10 tries, fail. Otherwise, output IP addresses
+counter=0
+while : ; do
+	sleep 60s
+	curl -s -o /dev/null http://archlinux.com
+	if [ $? -eq 1 ]; then
+		((counter++))
+		if [ "$counter" = 10 ]; then
+			echo "No Internet connection established after 10 minutes." >> /etc/issue
+			echo "Unable to get IP addresses." >> /etc/issue
+			exit 1
+		fi
+	else
+		#Output a list of all interfaces to /etc/issue
+		for interface in /sys/class/net/* ; do
+			baseInt=$(basename "$interface")
+			echo "IPv4 Address for $baseInt: \4{$baseInt}" >> /etc/issue
+		done
+		break
+	fi
 done
+
+#Finish by adding an extra newline to have the prompt one empty line down
 echo -e "\n" >> /etc/issue
