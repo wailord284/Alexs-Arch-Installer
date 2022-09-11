ccceac576d9d29767ecb87d631eedab837099ec2
Alex Gaudino <wailord284@gmail.com>
2022-09-10 22:34:38 -0700

    Bump limit of snapshots to 30

--- configs/scripts/snapperfirstboot.sh
+++ configs/scripts/snapperfirstboot.sh
@@ -4,7 +4,7 @@
 #Create our snapper config
 snapper -c root create-config /
 #Change snapper backups to keep to 25
-sed "s,NUMBER_LIMIT=\"50\",NUMBER_LIMIT=\"25\",g" -i /etc/snapper/configs/root
+sed "s,NUMBER_LIMIT=\"50\",NUMBER_LIMIT=\"30\",g" -i /etc/snapper/configs/root
 #Change snapper timeline settings to keep 0 hourly, 5 daily, 3 weekly and 1 monthly
 sed "s,TIMELINE_LIMIT_HOURLY=\"10\",TIMELINE_LIMIT_HOURLY=\"0\",g" -i /etc/snapper/configs/root
 sed "s,TIMELINE_LIMIT_DAILY=\"10\",TIMELINE_LIMIT_DAILY=\"5\",g" -i /etc/snapper/configs/root
