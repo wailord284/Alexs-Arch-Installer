098b22f8aab0a0c7c5cb0ab2ae521338fe33dc19
Alex Gaudino <wailord284@gmail.com>
2022-11-11 09:33:45 -0800

    Change daily snapshots to 3

--- configs/scripts/snapperfirstboot.sh
+++ configs/scripts/snapperfirstboot.sh
@@ -7,7 +7,7 @@ snapper -c root create-config /
 sed "s,NUMBER_LIMIT=\"50\",NUMBER_LIMIT=\"15\",g" -i /etc/snapper/configs/root
 #Change snapper timeline settings to keep 5 daily and 1 weekly
 sed "s,TIMELINE_LIMIT_HOURLY=\"10\",TIMELINE_LIMIT_HOURLY=\"0\",g" -i /etc/snapper/configs/root
-sed "s,TIMELINE_LIMIT_DAILY=\"10\",TIMELINE_LIMIT_DAILY=\"5\",g" -i /etc/snapper/configs/root
+sed "s,TIMELINE_LIMIT_DAILY=\"10\",TIMELINE_LIMIT_DAILY=\"3\",g" -i /etc/snapper/configs/root
 sed "s,TIMELINE_LIMIT_WEEKLY=\"0\",TIMELINE_LIMIT_WEEKLY=\"1\",g" -i /etc/snapper/configs/root
 sed "s,TIMELINE_LIMIT_MONTHLY=\"10\",TIMELINE_LIMIT_MONTHLY=\"0\",g" -i /etc/snapper/configs/root
 sed "s,TIMELINE_LIMIT_YEARLY=\"10\",TIMELINE_LIMIT_YEARLY=\"0\",g" -i /etc/snapper/configs/root
