9d81334637a85a4d2ab06e6c132157ba72ca62d0
Alex G <wailord284@gmail.com>
2023-09-24 23:30:25 -0700

    Update firefox configs

--- configs/firefox/k3cc1q7w.default-release/user.js
+++ configs/firefox/k3cc1q7w.default-release/user.js
@@ -8,45 +8,44 @@ user_pref("gfx.webrender.compositor", true);
 user_pref("layers.gpu-process.enabled", true);
 user_pref("media.hardware-video-decoding.enabled", true);
 user_pref("gfx.canvas.accelerated", true); // DEFAULT on macOS and Linux v.110
-user_pref("gfx.canvas.accelerated.cache-items", 32768);
-user_pref("gfx.canvas.accelerated.cache-size", 4096);
-user_pref("gfx.content.skia-font-cache-size", 80);
-user_pref("image.cache.size", 10485760);
-user_pref("image.mem.decode_bytes_at_a_time", 131072); // alt=65536; preferred=262144; chunk size for calls to the image decoders
-user_pref("image.mem.shared.unmap.min_expiration_ms", 120000); // default=60000; minimum timeout to unmap shared surfaces since they have been last used
-user_pref("media.cache_readahead_limit", 9000); // default=60; stop reading ahead when our buffered data is this many seconds ahead of the current playback
-user_pref("media.cache_resume_threshold", 6000); // default=30; when a network connection is suspended, don't resume it until the amount of buffered data falls below this threshold (in seconds)
-user_pref("browser.cache.memory.max_entry_size", 153600);
+user_pref("gfx.canvas.accelerated.cache-items", 4096); // default=2048; alt=8192
+user_pref("gfx.canvas.accelerated.cache-size", 512); // default=256; alt=1024
+user_pref("gfx.content.skia-font-cache-size", 20);
+user_pref("media.cache_readahead_limit", 7200); // 120 min; default=60; stop reading ahead when our buffered data is this many seconds ahead of the current playback
+user_pref("media.cache_resume_threshold", 3600); // 60 min; default=30; when a network connection is suspended, don't resume it until the amount of buffered data falls below this threshold
+user_pref("media.memory_cache_max_size", 65536); // default=8192; AF=65536; alt=131072
+user_pref("image.mem.decode_bytes_at_a_time", 32768); // default=16384; alt=65536; chunk size for calls to the image decoders
 user_pref("network.buffer.cache.size", 262144); // preferred=327680; default=32768
 user_pref("network.buffer.cache.count", 128); // preferred=240; default=24
-user_pref("network.ssl_tokens_cache_capacity", 32768); // default=2048; more TLS token caching (fast reconnects)
-user_pref("browser.startup.preXulSkeletonUI", false);
+user_pref("network.http.max-connections", 1800); // default=900
+user_pref("network.http.max-persistent-connections-per-server", 10); // default=6; download connections; anything above 10 is excessive
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5); // default=3
+user_pref("network.dnsCacheEntries", 1000); // default=400
+user_pref("network.ssl_tokens_cache_capacity", 10240); // default=2048; more TLS token caching (fast reconnects)
 
 //SECURITY - https://github.com/yokoffing/Betterfox/blob/master/Securefox.js
 user_pref("privacy.query_stripping.enabled", true); // enabled with "Strict"
-user_pref("privacy.query_stripping.strip_list", "__hsfp __hssc __hstc __s _hsenc _openstat dclid fbclid gbraid gclid hsCtaTracking igshid mc_eid ml_subscriber ml_subscriber_hash msclkid oft_c oft_ck oft_d oft_id oft_ids oft_k oft_lk oft_sk oly_anon_id oly_enc_id rb_clickid s_cid twclid vero_conv vero_id wbraid wickedid yclid");
+user_pref("privacy.purge_trackers.enabled", true); // DEFAULT
+user_pref("browser.send_pings", false); // DEFAULT
+user_pref("beacon.enabled", false);
 user_pref("browser.uitour.enabled", false);
-user_pref("privacy.query_stripping.enabled.pbmode", true);
-user_pref("security.OCSP.enabled", 0); // [DEFAULT: 1]
+user_pref("privacy.globalprivacycontrol.enabled", true);
+user_pref("privacy.globalprivacycontrol.functionality.enabled", true);
+user_pref("security.OCSP.enabled", 0);
 user_pref("security.remote_settings.crlite_filters.enabled", true);
 user_pref("security.pki.crlite_mode", 2);
 user_pref("security.cert_pinning.enforcement_level", 2);
 user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
 user_pref("browser.xul.error_pages.expert_bad_cert", true);
-user_pref("security.tls.enable_0rtt_data", false); // disable 0 RTT to improve tls 1.3 security
 user_pref("browser.privatebrowsing.forceMediaMemoryCache", true);
+user_pref("browser.sessionstore.interval", 60000); // 1 minute; default=15000 (15s)
 user_pref("privacy.history.custom", true);
-user_pref("security.insecure_connection_text.enabled", true);
-user_pref("security.insecure_connection_text.pbmode.enabled", true);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
 user_pref("network.IDN_show_punycode", true);
-user_pref("editor.truncate_user_pastes", false);
-user_pref("network.auth.subresource-http-auth-allow", 1);
-user_pref("media.peerconnection.ice.proxy_only_if_behind_proxy", true);
-user_pref("media.peerconnection.ice.default_address_only", true);
-user_pref("accessibility.force_disabled", 1);
-user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
-user_pref("geo.provider.use_gpsd", false); // [LINUX]
-user_pref("geo.provider.use_geoclue", false); // [FF102+] [LINUX]
+user_pref("dom.security.https_first", true);
+user_pref("dom.security.https_only_mode", true); // Normal + Private Browsing windows
 user_pref("toolkit.telemetry.unified", false);
 user_pref("toolkit.telemetry.enabled", false);
 user_pref("toolkit.telemetry.server", "data:,");
@@ -68,6 +67,7 @@ user_pref("browser.tabs.crashReporting.sendReport", false);
 user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 user_pref("app.normandy.enabled", false);
 user_pref("app.normandy.api_url", "");
+user_pref("browser.ping-centre.telemetry", false);
 user_pref("browser.newtabpage.activity-stream.telemetry", false);
 user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
 
@@ -80,29 +80,27 @@ user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families", 
 user_pref("browser.newtabpage.activity-stream.topSitesRows", 4);
 
 //ANNOYANCE - https://github.com/yokoffing/Betterfox/blob/master/Peskyfox.js
-user_pref("layout.css.prefers-color-scheme.content-override", 2);
-user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("layout.css.prefers-color-scheme.content-override", 0);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("app.update.suppressPrompts", true);
 user_pref("browser.compactmode.show", true);
 user_pref("browser.privatebrowsing.vpnpromourl", "");
+user_pref("browser.vpn_promo.enabled", false);
 user_pref("extensions.getAddons.showPane", false); // HIDDEN
 user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
 user_pref("browser.preferences.moreFromMozilla", false);
 user_pref("browser.tabs.tabmanager.enabled", false);
-user_pref("full-screen-api.transition-duration.enter", "0 0");
-user_pref("full-screen-api.transition-duration.leave", "0 0");
-user_pref("full-screen-api.warning.delay", -1);
-user_pref("full-screen-api.warning.timeout", 0);
-user_pref("browser.aboutwelcome.enabled", false);
-user_pref("findbar.highlightAll", true);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.aboutwelcome.enabled", false); // disable Intro screens
+user_pref("browser.translations.enable", true);
 user_pref("browser.urlbar.suggest.calculator", true);
 user_pref("browser.urlbar.unitConversion.enabled", true);
-user_pref("browser.urlbar.suggest.topsites", false);
-user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false); // Recommended by Pocket
 user_pref("extensions.pocket.enabled", false);
-user_pref("browser.download.always_ask_before_handling_new_types", true);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false); // Shortcuts
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false); // Recommended by Pocket
+user_pref("browser.download.manager.addToRecentDocs", false);
+user_pref("browser.download.alwaysOpenPanel", false);
 user_pref("browser.download.open_pdf_attachments_inline", true);
-user_pref("layout.css.has-selector.enabled", true);
+user_pref("pdfjs.sidebarViewOnLoad", 2);
+user_pref("browser.tabs.loadBookmarksInTabs", true);
